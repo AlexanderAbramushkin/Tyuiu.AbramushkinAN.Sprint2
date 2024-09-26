d24fa7786f04aa78f956d75e3cc81a2949919d2f
Tyuiu.AbramushkinAN@outlook.com <Tyuiu.AbramushkinAN@outlook.com>
2024-09-26 21:46:15 +0500

    Task2

---
+++ Tyuiu.AbramushkinAN.Sprint2.Task2.V7.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+using Tyuiu.AbramushkinAN.Sprint2.Task2.V7.Lib;
+namespace Tyuiu.AbramushkinAN.Sprint2.Task2.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Test_Dotintinarea()
+        {
+            DataService ds = new DataService();
+            int x = 4;
+            int y = 3;
+            bool wait = true;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void Test_Dotnotinarea()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            int y = 2;
+            bool wait = false;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AbramushkinAN.Sprint2.Task2.V7/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Tyuiu.AbramushkinAN.Sprint2.Task2.V7.Lib;
+namespace Tyuiu.AbramushkinAN.Sprint2.Task2.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Абрамушкин А. Н. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор if - полная и короткая форма записи                      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Абрамушкин Александр Николаевич | ИИПб-24-2                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на, которая запрашивает целые значения с клавиатуры  *");
+            Console.WriteLine("* и вычисляет находится ли точка с координатами X,Y                       *");
+            Console.WriteLine("* в заштрихованной области.                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            
+            Console.WriteLine("Введите значение X:");
+            int x=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            if (res == true)
+            {
+                Console.WriteLine("Точка находится В закрашенной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка находится ВНЕ закрашенной области");
+            }
+        }
+    }
+}
