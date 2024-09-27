81819718cc71f1becc94644baea3134a17c2894c
Tyuiu.AbramushkinAN@outlook.com <Tyuiu.AbramushkinAN@outlook.com>
2024-09-27 21:25:10 +0500

    Task3

---
+++ Tyuiu.AbramushkinAN.Sprint2.Task3.V29.Test/DataServiceTest.cs
@@ -0,0 +1,48 @@
+using Tyuiu.AbramushkinAN.Sprint2.Task3.V29.Lib;
+
+namespace Tyuiu.AbramushkinAN.Sprint2.Task3.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double wait = 486;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double wait = 1;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -20;
+            double wait = 0.951;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -34;
+            double wait = -373.971;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AbramushkinAN.Sprint2.Task3.V29/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.AbramushkinAN.Sprint2.Task3.V29.Lib;
+
+namespace Tyuiu.AbramushkinAN.Sprint2.Task3.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Абрамушкин А. Н. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Абрамушкин Александр Николаевич | ИИПб-24-2                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с    *");
+            Console.WriteLine("* использованием вложенных оператор if-else, где пользователь вводит      *");
+            Console.WriteLine("* значение переменной X с клавиатуры. Округлить полученное значение до    *");
+            Console.WriteLine("* трех знаков после запятой;                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x));
+        }
+    }
+}
