cb1c58f11104fb6cdb4af1304d33db101108fb8c
Tyuiu.AbramushkinAN@outlook.com <Tyuiu.AbramushkinAN@outlook.com>
2024-10-04 20:15:03 +0500

    Task5

---
+++ Tyuiu.AbramushkinAN.Sprint2.Task5.V2.Lib/DataService.cs
@@ -0,0 +1,54 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.AbramushkinAN.Sprint2.Task5.V2.Lib
+{
+    public class DataService : ISprint2Task5V2
+    {
+        public string FindMonthSeason(int value)
+        {
+            string TimeOfYear = "";
+            switch (value) 
+            {
+                case 12:
+                    TimeOfYear = "Зима";
+                    break;
+                case 1:
+                    TimeOfYear = "Зима";
+                    break;
+                case 2:
+                    TimeOfYear = "Зима";
+                    break;
+                case 3:
+                    TimeOfYear = "Весна";
+                    break;
+                case 4:
+                    TimeOfYear = "Весна";
+                    break;
+                case 5:
+                    TimeOfYear = "Весна";
+                    break;
+                case 6:
+                    TimeOfYear = "Лето";
+                    break;
+                case 7:
+                    TimeOfYear = "Лето";
+                    break;
+                case 8:
+                    TimeOfYear = "Лето";
+                    break;
+                case 9:
+                    TimeOfYear = "Осень";
+                    break;
+                case 10:
+                    TimeOfYear = "Осень";
+                    break;
+                case 11:
+                    TimeOfYear = "Осень";
+                    break;
+                default:
+                    throw new ArgumentException($"Месяц должен быть от 1 до 12. Значение {value} не соответствует условию");
+            }
+            return TimeOfYear;
+        }
+    }
+}
---
+++ Tyuiu.AbramushkinAN.Sprint2.Task5.V2/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.AbramushkinAN.Sprint2.Task5.V2.Lib;
+namespace Tyuiu.AbramushkinAN.Sprint2.Task5.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Абрамушкин А. Н. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Абрамушкин Александр Николаевич | ИИПб-24-2                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Условие: По данному месяцу, определите в какую пору года попадает этот  *");
+            Console.WriteLine("* месяц (Зима, Лето, Весна, Осень).                                       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер месяца:");
+            int NumberOfMonth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.FindMonthSeason(NumberOfMonth));
+        }
+    }
+}
